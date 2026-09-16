e888ab31f5cbe3aee48307982bd746108c812394
omkarbhanusghare9989-afk <omkarbhanusghare9989@gmail.com>
2026-09-16 23:54:00 +0530

    initial commit

---
+++ DeluxPizza.java
@@ -0,0 +1,15 @@
+package PizzaBillGenerator;
+
+public class DeluxPizza extends Pizza{
+
+	public DeluxPizza(Boolean veg) {
+		super(veg);
+		super.addExtraCheese();
+		super.addExtraToppings();
+	}
+	
+	public void addExtraCheese() {}
+	
+	public void addExtraToppings() {}
+
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+package PizzaBillGenerator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pizza basePizza = new Pizza(true);
+		basePizza.addExtraToppings();
+		basePizza.addExtraCheese();
+		basePizza.takeAway();
+		basePizza.getBill();
+		
+		DeluxPizza dp = new DeluxPizza(false);
+		dp.takeAway();
+		dp.getBill();
+	}
+
+}
